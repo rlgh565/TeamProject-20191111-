8d11d0c074e2dd00b33febd1fcf665a8f4e99fc0
rlgh565 <rlgh565@gmail.com>
2019-11-11 13:52:34 +0900

    Frame 수정

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -4,7 +4,7 @@ import java.awt.*;
 /**
  * 여기에 MyHelloFrame 클래스 설명을 작성하십시오.
  * 
- * @author (2018210025 현기호, 2018315053 테라오카유이카 ) 
+ * @author (2018210025 현기호,2018315053 테라오카유이카 ) 
  * @version (2019.11.11)
  */
 public class MyHelloFrame extends JFrame
