ac37e026e2a96b312c0e79766c850396d63d3e5d
rlgh565 <rlgh565@gmail.com>
2019-11-11 13:07:01 +0900

    MyHelloPanelListener 클래스 구현

---
+++ MyHelloPanelListener.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ * Write a description of class MyHelloPanelListener here.
+ *
+ * @author (2018210025현기호,2018315053테라오카 유이카)
+ * @version (20191111)
+ */
+public class MyHelloPanelListener extends MouseAdapter 
+                                  implements KeyListener{
+    public JPanel mp = new JPanel();
+    public void keyPressed(KeyEvent e){
+        
+    }
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
+    public void mousePressed(MouseEvent e){
+        
+    }
+    
+}
